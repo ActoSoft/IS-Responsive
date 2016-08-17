bb3711ee18137fcc0e0d4335ce8aea547c629743
MartinMelo97 <nitram-210397@hotmail.com>
2016-08-16 22:03:05 -0500

    asdf

---
+++ js/main2.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+	$(".slider").slider({
+		interval:10000,
+		indicators:true,
+		transition:800
+
+	});
+
+
+	$(".button-collapse").sideNav();
+	$(".dropdown-button").dropdown({
+		hover: true,
+		belowOrigin: true,
+		constrain_width: false
+	});
+	/*$(".nav").css({ "background-color":"#333333",
+									"box-shadow":"none",
+									"height": "100px"
+										});
+	$(".nav-li").css({"padding-top": "20px"});
+	*/
+});
+
+/*
+$(document).on("scroll", function(){
+		var scrollTop = $(document).scrollTop();
+		if(scrollTop > 300){
+			$(".nav").css({ "background-color":"#333333",
+											"box-shadow":"0px 3px 5px rgba(0,0,0,.45)",
+											"height": "64px"
+										});
+			$(".nav-li").css({"padding-top": "0px"});
+		}else{
+			$(".nav").css({ "background-color":"#333333",
+									"box-shadow":"none",
+									"height": "100px"
+										});
+			$(".nav-li").css({"padding-top": "20px"});
+		}
+});
+
+*/
+
+$(document).on("scroll", function(){
+
+	var scrollTop = $(document).scrollTop();
+	if(scrollTop > 200){
+		$('a.ancla').fadeIn('slow');
+		/*$(".ancla").css({
+			"display":"block"
+		});*/
+	}
+	else{
+		$('a.ancla').fadeOut('slow');
+		/*$(".ancla").css({
+			"display":"none"*/
+		}
+});
+
+/*$('a.ancla').click(function(){
+	$('html, body').animate({
+		scrollTop: 0
+	}, 700);
+
+	return false;
+});
+*/
+function gototop(){
+	$('html, body').animate({
+		scrollTop: 0	
+	}, 500);
+}
---
+++ js/random-number.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+	var number = Math.floor(Math.random()* 9000) + 1000;
+	document.getElementById("number").value = number;
+
+
+});
+
+function CompareNumbers() {
+
+	var original = document.getElementById('number').value;
+	var inputted = document.getElementById('code').value;
+
+	if(original == inputted)
+	{
+		alert('Los numeros coinciden!');
+		//window.location.assign('../contacto.html');
+	}
+	else
+	{
+		alert('Los números no coinciden, inténtalo nuevamente')
+		var number = Math.floor(Math.random()* 9000) + 1000;
+		document.getElementById("number").value = number;
+		document.getElementById("code").value = null;
+		//window.location.assign('../contacto.html');
+	}
+
+}
