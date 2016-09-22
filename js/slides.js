5ac405c91ff3d292c7bdbf27121b1963653e0455
MartinMelo97 <nitram-210397@hotmail.com>
2016-09-21 23:49:43 -0500

    Arreglamos slider de principal

---
+++ js/slides.js
@@ -0,0 +1,17 @@
+$(function(){
+
+	$('.slides2').slidesjs({	
+	height: 300,
+	play: {
+		active:false,
+		effect:"slide",
+		interval:4000,
+		auto:true,
+		swap:true,
+		pauseOnHover: false,
+		restartDelay: 2500,
+	}
+
+	});
+
+});
